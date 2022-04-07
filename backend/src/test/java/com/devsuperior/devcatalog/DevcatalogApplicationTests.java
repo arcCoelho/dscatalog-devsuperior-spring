9d3c968b0d29c58b6ca9bef9262dd65d08e29f5a
allan coelho <arc-coelho@hotmail.com>
2022-04-07 19:29:42 -0300

    backend project created

---
+++ backend/src/test/java/com/devsuperior/devcatalog/DevcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
